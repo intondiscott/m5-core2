7959731648b42519cd9d05b3a64b8b94550b1354
Scotty <73749875+intondiscott@users.noreply.github.com>
2024-12-07 18:00:35 -0500

    Update new UI

--- src/main.cpp
+++ src/main.cpp
@@ -7,177 +7,8 @@ M5Display tft;
 const int SCREEN_WIDTH = 320;
 const int SCREEN_HEIGHT = 240;
 unsigned long lastTickMillis = 0;
-
-
-/**
- * the example show the use of cubic-bezier3 in animation.
- * the control point P1,P2 of cubic-bezier3 can be adjusted by slider.
- * and the chart shows the cubic-bezier3 in real time. you can click
- * run button see animation in current point of cubic-bezier3.
- */
-
-#define CHART_POINTS_NUM 256
-
-struct {
-    lv_obj_t * anim_obj;
-    lv_obj_t * chart;
-    lv_chart_series_t * ser1;
-    lv_obj_t * p1_slider;
-    lv_obj_t * p1_label;
-    lv_obj_t * p2_slider;
-    lv_obj_t * p2_label;
-    lv_obj_t * run_btn;
-    uint16_t p1;
-    uint16_t p2;
-    lv_anim_t a;
-} ginfo;
-
-
-static int32_t anim_path_bezier3_cb(const lv_anim_t * a);
-static void refer_chart_cubic_bezier(void);
-static void run_button_event_handler(lv_event_t * e);
-static void slider_event_cb(lv_event_t * e);
-static void page_obj_init(lv_obj_t * par);
-static void anim_x_cb(void * var, int32_t v);
-
-/**
- * create an animation
- */
-void lv_example_anim_3(void)
-{
-    static int32_t col_dsc[] = {LV_GRID_FR(1), 200, LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
-    static int32_t row_dsc[] = {30, 10, 10, LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
-
-    /*Create a container with grid*/
-    lv_obj_t * cont = lv_obj_create(lv_screen_active());
-    lv_obj_set_style_pad_all(cont, 2, LV_PART_MAIN);
-    lv_obj_set_style_pad_column(cont, 10, LV_PART_MAIN);
-    lv_obj_set_style_pad_row(cont, 10, LV_PART_MAIN);
-    lv_obj_set_grid_dsc_array(cont, col_dsc, row_dsc);
-    lv_obj_set_size(cont, 320, 240);
-    lv_obj_center(cont);
-
-    page_obj_init(cont);
-
-    lv_anim_init(&ginfo.a);
-    lv_anim_set_var(&ginfo.a, ginfo.anim_obj);
-    int32_t end = lv_obj_get_style_width(cont, LV_PART_MAIN) -
-                  lv_obj_get_style_width(ginfo.anim_obj, LV_PART_MAIN) - 10;
-    lv_anim_set_values(&ginfo.a, 5, end);
-    lv_anim_set_duration(&ginfo.a, 2000);
-    lv_anim_set_exec_cb(&ginfo.a, anim_x_cb);
-    lv_anim_set_path_cb(&ginfo.a, anim_path_bezier3_cb);
-
-    refer_chart_cubic_bezier();
-}
-
-static int32_t anim_path_bezier3_cb(const lv_anim_t * a)
-{
-    uint32_t t = lv_map(a->act_time, 0, a->duration, 0, 1024);
-    int32_t step = lv_bezier3(t, 0, ginfo.p1, ginfo.p2, 1024);
-    int32_t new_value;
-    new_value = step * (a->end_value - a->start_value);
-    new_value = new_value >> 10;
-    new_value += a->start_value;
-    return new_value;
-}
-
-static void refer_chart_cubic_bezier(void)
-{
-    for(uint16_t i = 0; i <= CHART_POINTS_NUM; i ++) {
-        uint32_t t = i * (1024 / CHART_POINTS_NUM);
-        int32_t step = lv_bezier3(t, 0, ginfo.p1, ginfo.p2, 1024);
-        lv_chart_set_value_by_id2(ginfo.chart, ginfo.ser1, i, t, step);
-    }
-    lv_chart_refresh(ginfo.chart);
-}
-
-static void anim_x_cb(void * var, int32_t v)
-{
-  
-   
-    lv_obj_set_style_translate_x((lv_obj_t*)var, v, LV_PART_MAIN);
-}
-
-static void run_button_event_handler(lv_event_t * e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    if(code == LV_EVENT_CLICKED) {
-        lv_anim_start(&ginfo.a);
-    }
-}
-
-static void slider_event_cb(lv_event_t * e)
-{
-    char buf[16];
-    lv_obj_t * label;
-  
-    lv_obj_t * slider =  (lv_obj_t*)lv_event_get_target(e);
-
-    if(slider == ginfo.p1_slider) {
-        label = ginfo.p1_label;
-        ginfo.p1 = lv_slider_get_value(slider);
-        lv_snprintf(buf, sizeof(buf), "p1:%d", ginfo.p1);
-    }
-    else {
-        label = ginfo.p2_label;
-        ginfo.p2 = lv_slider_get_value(slider);
-        lv_snprintf(buf, sizeof(buf), "p2:%d", ginfo.p2);
-    }
-
-    lv_label_set_text(label, buf);
-    refer_chart_cubic_bezier();
-}
-
-static void page_obj_init(lv_obj_t * par)
-{
-    ginfo.anim_obj = lv_obj_create(par);
-    lv_obj_set_size(ginfo.anim_obj, 30, 30);
-    lv_obj_set_align(ginfo.anim_obj, LV_ALIGN_TOP_LEFT);
-    lv_obj_remove_flag(ginfo.anim_obj, LV_OBJ_FLAG_SCROLLABLE);
-    lv_obj_set_style_bg_color(ginfo.anim_obj, lv_palette_main(LV_PALETTE_RED), LV_PART_MAIN);
-    lv_obj_set_grid_cell(ginfo.anim_obj, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 0, 1);
-
-    ginfo.p1_label = lv_label_create(par);
-    ginfo.p2_label = lv_label_create(par);
-    lv_label_set_text(ginfo.p1_label, "p1:0");
-    lv_label_set_text(ginfo.p2_label, "p2:0");
-    lv_obj_set_grid_cell(ginfo.p1_label, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 1, 1);
-    lv_obj_set_grid_cell(ginfo.p2_label, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 2, 1);
-
-    ginfo.p1_slider = lv_slider_create(par);
-    ginfo.p2_slider = lv_slider_create(par);
-    lv_slider_set_range(ginfo.p1_slider, 0, 1024);
-    lv_slider_set_range(ginfo.p2_slider, 0, 1024);
-    lv_obj_set_style_pad_all(ginfo.p1_slider, 2, LV_PART_KNOB);
-    lv_obj_set_style_pad_all(ginfo.p2_slider, 2, LV_PART_KNOB);
-    lv_obj_add_event_cb(ginfo.p1_slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);
-    lv_obj_add_event_cb(ginfo.p2_slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);
-    lv_obj_set_grid_cell(ginfo.p1_slider, LV_GRID_ALIGN_STRETCH, 1, 1, LV_GRID_ALIGN_START, 1, 1);
-    lv_obj_set_grid_cell(ginfo.p2_slider, LV_GRID_ALIGN_STRETCH, 1, 1, LV_GRID_ALIGN_START, 2, 1);
-
-    ginfo.run_btn = lv_button_create(par);
-    lv_obj_add_event_cb(ginfo.run_btn, run_button_event_handler, LV_EVENT_CLICKED, NULL);
-    lv_obj_t * btn_label = lv_label_create(ginfo.run_btn);
-    lv_label_set_text(btn_label, LV_SYMBOL_PLAY);
-    lv_obj_center(btn_label);
-    lv_obj_set_grid_cell(ginfo.run_btn, LV_GRID_ALIGN_STRETCH, 2, 1, LV_GRID_ALIGN_STRETCH, 1, 2);
-
-    ginfo.chart = lv_chart_create(par);
-    lv_obj_set_style_pad_all(ginfo.chart, 0, LV_PART_MAIN);
-    lv_obj_set_style_size(ginfo.chart, 0, 0, LV_PART_INDICATOR);
-    lv_chart_set_type(ginfo.chart, LV_CHART_TYPE_SCATTER);
-    ginfo.ser1 = lv_chart_add_series(ginfo.chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
-    lv_chart_set_range(ginfo.chart, LV_CHART_AXIS_PRIMARY_Y, 0, 1024);
-    lv_chart_set_range(ginfo.chart, LV_CHART_AXIS_PRIMARY_X, 0, 1024);
-    lv_chart_set_point_count(ginfo.chart, CHART_POINTS_NUM);
-    lv_obj_set_grid_cell(ginfo.chart, LV_GRID_ALIGN_STRETCH, 0, 3, LV_GRID_ALIGN_STRETCH, 3, 1);
-}
-
-
-    
-
-  
+char *bat = (char*)malloc(1*sizeof(char));
+ 
 void my_touch_read(lv_indev_t *indev_driver,lv_indev_data_t *data)
 {
   uint16_t touchX, touchY;
@@ -210,19 +41,57 @@ void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *color_p)
   lv_disp_flush_ready(disp);
 }
 
-static void event_handler(lv_event_t * e)
-{
-   static lv_obj_t * btn;
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * btn1 = lv_button_create(btn);
-    if(code == LV_EVENT_CLICKED) {
-        static uint8_t cnt = 0;
-        cnt++;
 
-        /*Get the first child of the button which is the label and change its text*/
-        lv_obj_t * label = lv_obj_get_child(btn1, 0);
-        lv_label_set_text_fmt(label, "Button: %d", cnt);
-    }
+
+/**
+ * update info on m5 core2
+ */
+void screen_update(lv_timer_t *timer)
+{
+    
+    RTC_TimeTypeDef time_t;
+    uint8_t battery_percentage = m5.Axp.GetBatteryLevel();
+    lv_obj_t *main_screen,
+             *nav_screen,
+             *battery_label,
+             *datetime_label,
+             *bat_img; 
+    main_screen = lv_obj_create(lv_screen_active()); 
+    nav_screen = lv_obj_create(main_screen);
+    
+    lv_obj_set_size(main_screen,SCREEN_WIDTH,SCREEN_HEIGHT);
+    lv_obj_center(main_screen);
+    lv_obj_set_flex_flow(main_screen,LV_FLEX_FLOW_ROW);
+    lv_obj_set_height(nav_screen,30);
+    lv_obj_set_flex_grow(nav_screen,1);
+    battery_label = lv_label_create(nav_screen);
+    datetime_label = lv_label_create(nav_screen);
+    bat_img = lv_img_create(nav_screen);
+    lv_image_set_src(bat_img,"/assets/IMG_4397.c");
+    snprintf(bat, sizeof(bat),"%d",battery_percentage);
+    lv_obj_align(battery_label,LV_ALIGN_RIGHT_MID,0,0);
+    lv_obj_align(datetime_label,LV_ALIGN_LEFT_MID,0,0);
+    lv_obj_align(bat_img,LV_ALIGN_CENTER,0,0);
+    lv_obj_set_style_pad_all(nav_screen,0,LV_PART_MAIN);
+    lv_obj_set_style_pad_all(main_screen,0,LV_PART_MAIN);
+    lv_obj_set_style_margin_all(nav_screen,0,LV_PART_MAIN);
+    lv_obj_set_style_bg_color(main_screen, lv_color_hex(0x6b8bbf), LV_PART_MAIN);
+    lv_label_set_text_fmt(battery_label,"%s%%", bat);
+    M5.Rtc.GetTime(&time_t);
+    lv_label_set_text_fmt(
+      datetime_label,"%02d : %02d : %02d %s", 
+      time_t.Hours>12?time_t.Hours-12:time_t.Hours, 
+      time_t.Minutes, 
+      time_t.Seconds,
+      time_t.Hours >= 12? "PM":"AM");
+    
+    
+}
+
+void drawUI(){
+  
+  lv_timer_t *timer = lv_timer_create(screen_update,1000,nullptr); 
+  free(bat);
 }
 
 void setupLVGL(){
@@ -241,7 +110,7 @@ void setupLVGL(){
   lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);  /*Touch pad is a pointer-like device*/
   lv_indev_set_read_cb(indev, my_touch_read);
 
-    lv_example_anim_3();
+  drawUI();  
    
 
 }
@@ -251,6 +120,7 @@ void setup() {
   Serial.begin(115200);
 
   M5.begin();
+  M5.Rtc.begin();
   tft.setRotation(1);
   
   tft.setBrightness(200);
