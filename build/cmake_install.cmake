8315b6be95c410d6806404dfee616cd7e78f7bf0
= <=>
2024-12-10 15:30:38 -0500

    refactored code

---
+++ build/cmake_install.cmake
@@ -0,0 +1,180 @@
+# Install script for directory: C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/lvgl")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files/lvgl/include/lvgl//src")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files/lvgl/include/lvgl/" TYPE DIRECTORY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/src" FILES_MATCHING REGEX "/[^/]*\\.h$")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files/lvgl/include/lvgl/lv_version.h;C:/Program Files/lvgl/include/lvgl/lvgl.h;C:/Program Files/lvgl/include/lvgl/lv_conf.h")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files/lvgl/include/lvgl" TYPE FILE FILES
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_version.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lvgl.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_conf.h"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files/lvgl/include/lvgl//examples")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files/lvgl/include/lvgl/" TYPE DIRECTORY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/examples" FILES_MATCHING REGEX "/[^/]*\\.h$")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files/lvgl/include/lvgl//demos")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files/lvgl/include/lvgl/" TYPE DIRECTORY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/demos" FILES_MATCHING REGEX "/[^/]*\\.h$")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lvgl.pc")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/Debug/lvgl.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/Release/lvgl.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/MinSizeRel/lvgl.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/RelWithDebInfo/lvgl.lib")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/lvgl" TYPE FILE FILES
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_version.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lvgl.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_conf.h"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/Debug/lvgl_thorvg.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/Release/lvgl_thorvg.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/MinSizeRel/lvgl_thorvg.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/RelWithDebInfo/lvgl_thorvg.lib")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/lvgl" TYPE FILE FILES
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_version.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lvgl.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_conf.h"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/Debug/lvgl_demos.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/Release/lvgl_demos.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/MinSizeRel/lvgl_demos.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/RelWithDebInfo/lvgl_demos.lib")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/lvgl" TYPE FILE FILES
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_version.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lvgl.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_conf.h"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/Debug/lvgl_examples.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/Release/lvgl_examples.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/MinSizeRel/lvgl_examples.lib")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/inton/OneDrive/Desktop/m5-core2/build/lib/RelWithDebInfo/lvgl_examples.lib")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/lvgl" TYPE FILE FILES
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_version.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lvgl.h"
+    "C:/Users/inton/OneDrive/Desktop/m5-core2/lib/lvgl/lv_conf.h"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "C:/Users/inton/OneDrive/Desktop/m5-core2/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,8 @@ struct {
               *low_bat_img,
               *button_text,
               *charging_img,
-              *settings_icon;
-          char *bat = (char*)malloc(sizeof(char));
+              *button[3];
+          char bat[4];
 } M5DisplayUI;   
 void my_touch_read(lv_indev_t *indev_driver,lv_indev_data_t *data)
 {
@@ -68,23 +68,14 @@ void screen_update(lv_timer_t *timer)
     RTC_TimeTypeDef time_t;
     uint8_t battery_percentage = m5.Axp.GetBatteryLevel();
     
-
-    M5DisplayUI.main_screen = lv_obj_create(lv_screen_active()); 
-    M5DisplayUI.nav_screen = lv_obj_create(M5DisplayUI.main_screen);
     LV_IMAGE_DECLARE(battery_icon);
     LV_IMAGE_DECLARE(low_battery);
     LV_IMAGE_DECLARE(charging);
-    lv_obj_set_size(M5DisplayUI.main_screen,SCREEN_WIDTH,SCREEN_HEIGHT);
-    lv_obj_center(M5DisplayUI.main_screen);
-    lv_obj_set_flex_flow(M5DisplayUI.main_screen,LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_size(M5DisplayUI.nav_screen,SCREEN_WIDTH,30);
-    
-    M5DisplayUI.battery_label = lv_label_create(M5DisplayUI.nav_screen);
-    M5DisplayUI.datetime_label = lv_label_create(M5DisplayUI.nav_screen);
     M5DisplayUI.bat_img = lv_img_create(M5DisplayUI.nav_screen);
     M5DisplayUI.low_bat_img = lv_img_create(M5DisplayUI.nav_screen);
     M5DisplayUI.charging_img = lv_img_create(M5DisplayUI.nav_screen);
-    if(M5.Axp.GetAPSVoltage() > 4.9)
+    
+    if(M5.Axp.GetAPSVoltage() > 4.8)
     {
       lv_image_set_src(M5DisplayUI.charging_img,&charging);
       lv_obj_align(M5DisplayUI.charging_img,LV_ALIGN_RIGHT_MID,0,0);
@@ -102,25 +93,7 @@ void screen_update(lv_timer_t *timer)
     
     
     snprintf(M5DisplayUI.bat, sizeof(M5DisplayUI.bat),"%d",battery_percentage);
-    lv_obj_align(M5DisplayUI.battery_label,LV_ALIGN_RIGHT_MID,-30,0);
-    lv_obj_align(M5DisplayUI.datetime_label,LV_ALIGN_LEFT_MID,0,0);
     
-    for(int i = 0; i < 10; i++){
-      lv_obj_set_flex_flow(M5DisplayUI.main_screen,LV_FLEX_FLOW_ROW_WRAP);
-      M5DisplayUI.settings_icon = lv_button_create(M5DisplayUI.main_screen);
-    M5DisplayUI.button_text = lv_label_create(M5DisplayUI.settings_icon);
-    lv_obj_align(M5DisplayUI.button_text,LV_ALIGN_CENTER,0,0);
-    lv_label_set_text(M5DisplayUI.button_text,"Setting");
-    lv_obj_set_size(M5DisplayUI.settings_icon,80,50);
-    }
-  
-    
-    lv_obj_set_style_pad_all(M5DisplayUI.nav_screen,0,LV_PART_MAIN);
-    lv_obj_set_style_pad_all(M5DisplayUI.main_screen,0,LV_PART_MAIN);
-    lv_obj_set_style_margin_all(M5DisplayUI.nav_screen,0,LV_PART_MAIN);
-    lv_obj_set_style_bg_color(M5DisplayUI.main_screen, lv_color_hex(0x98a3a2), LV_PART_MAIN);
-    lv_obj_set_style_bg_color(M5DisplayUI.nav_screen, lv_color_hex(0x000000), LV_PART_MAIN);
-    lv_obj_set_style_text_color(M5DisplayUI.nav_screen, lv_color_hex(0xffffff), LV_PART_MAIN);
     lv_label_set_text_fmt(M5DisplayUI.battery_label,"%s%%", M5DisplayUI.bat);
     M5.Rtc.GetTime(&time_t);
     lv_label_set_text_fmt(
@@ -130,11 +103,45 @@ void screen_update(lv_timer_t *timer)
       time_t.Seconds,
       time_t.Hours >= 12? "PM":"AM");   
 }
-
+void event_cb(lv_event_t *e){
+    static uint32_t cnt = 1;
+    lv_obj_t * btn = (lv_obj_t*)lv_event_get_target(e);
+    lv_obj_t * label = lv_obj_get_child(btn, 0);
+    lv_label_set_text_fmt(label, "%"LV_PRIu32, cnt);
+    cnt++;
+}
 void drawUI(){
   
-  lv_timer_t *timer = lv_timer_create(screen_update,1000,nullptr);  
-  free(M5DisplayUI.bat);
+  lv_timer_t *timer = lv_timer_create(screen_update,1000,nullptr); 
+
+  M5DisplayUI.main_screen = lv_obj_create(lv_screen_active()); 
+  M5DisplayUI.nav_screen = lv_obj_create(M5DisplayUI.main_screen);
+  M5DisplayUI.battery_label = lv_label_create(M5DisplayUI.nav_screen);
+  M5DisplayUI.datetime_label = lv_label_create(M5DisplayUI.nav_screen);
+
+  lv_obj_set_size(M5DisplayUI.main_screen,SCREEN_WIDTH,SCREEN_HEIGHT);
+  lv_obj_center(M5DisplayUI.main_screen);
+  lv_obj_set_flex_flow(M5DisplayUI.main_screen,LV_FLEX_FLOW_COLUMN);
+  lv_obj_set_size(M5DisplayUI.nav_screen,SCREEN_WIDTH,30);
+  lv_obj_align(M5DisplayUI.battery_label,LV_ALIGN_RIGHT_MID,-30,0);
+  lv_obj_align(M5DisplayUI.datetime_label,LV_ALIGN_LEFT_MID,0,0);
+    
+  lv_obj_set_style_pad_all(M5DisplayUI.nav_screen,0,LV_PART_MAIN);
+  lv_obj_set_style_pad_all(M5DisplayUI.main_screen,0,LV_PART_MAIN);
+  lv_obj_set_style_margin_all(M5DisplayUI.nav_screen,0,LV_PART_MAIN);
+  lv_obj_set_style_bg_color(M5DisplayUI.main_screen, lv_color_hex(0x98a3a2), LV_PART_MAIN);
+  lv_obj_set_style_bg_color(M5DisplayUI.nav_screen, lv_color_hex(0x000000), LV_PART_MAIN);
+  lv_obj_set_style_text_color(M5DisplayUI.nav_screen, lv_color_hex(0xffffff), LV_PART_MAIN);
+    
+  
+for (int i =0; i < 3;i++){
+    M5DisplayUI.button[i] = lv_button_create(M5DisplayUI.main_screen);
+    M5DisplayUI.button_text = lv_label_create(M5DisplayUI.button[i]);
+    lv_label_set_text(M5DisplayUI.button_text,"0");
+    lv_obj_set_size(M5DisplayUI.button_text,80,40);
+    
+    lv_obj_add_event_cb(M5DisplayUI.button[i],event_cb,LV_EVENT_CLICKED,nullptr);
+}
 }
 
 void setupLVGL(){
